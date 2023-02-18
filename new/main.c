20f8c1db4cfee480ae929743d283e305999ce680
dwexel <dexterearley@gmail.com>
2023-02-18 00:42:58 -0600

    fixed

--- new/main.c
+++ new/main.c
@@ -9,47 +9,28 @@
 #include<winsock2.h>
 
 #define PORT "8888"
-
 #define DEFAULT_BUFLEN 512
 
+#define filename "index.html"
 
-int err();
+// int err();
+
+// print to standard error
+
 void setHttpHeader(char httpHeader[]);
-
-// void report(struct sockaddr_in *serverAddress);
-
-void report(struct sockaddr *serverAddress)
-{
-   char hostBuffer[INET6_ADDRSTRLEN];
-   char serviceBuffer[NI_MAXSERV];
-
-	int err = getnameinfo(
-		serverAddress, (socklen_t)sizeof(*serverAddress),
-		hostBuffer,    sizeof(hostBuffer), 
-		serviceBuffer, sizeof(serviceBuffer),
-		NI_NUMERICHOST
-	);
-
-   if (err != 0) 
-	{
-        printf("It's not working!!\n");
-   } 
-	else 
-	{
-	    printf("\n\nServer listening on http://%s:%s\n", hostBuffer, serviceBuffer);
-	}
-}
-
-
+void report(struct sockaddr *serverAddress);
 
 int main(int argc , char *argv[])
 {
 	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2,2), &wsa) != 0)
+
+	int iResult = WSAStartup(MAKEWORD(2,2), &wsa);
+	if (iResult != 0) 
 	{
-		exit(err());
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
 	}
-	
+
 	// get server/host address data
 	struct addrinfo hints;
 	struct addrinfo *result = NULL;
@@ -63,7 +44,9 @@ int main(int argc , char *argv[])
 
 	if (getaddrinfo(NULL, PORT, &hints, &result) != 0)
 	{
-		exit(err());
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
 	}
 		
 	// Create a SOCKET for the server to listen for client connections
@@ -95,73 +78,71 @@ int main(int argc , char *argv[])
 		closesocket(ListenSocket);
 		WSACleanup();
 		return 1;
-	}	
-
-	// Accept a client socket
-	SOCKET ClientSocket = INVALID_SOCKET;
-	if ((ClientSocket = accept(ListenSocket, NULL, NULL)) == INVALID_SOCKET) 
-	{
-		printf("accept failed: %d\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
 	}
-	
+
 	char httpHeader[8000] = "HTTP/1.1 200 OK\r\n\n";
 	setHttpHeader(httpHeader);
-	report(result->ai_addr);
 
-	// input char
-	unsigned char a;
+	SOCKET ClientSocket = INVALID_SOCKET;
 
 	char recvbuf[DEFAULT_BUFLEN];
 	int sendResult;
-	int recieveResult;
-	// int recvbuflen = DEFAULT_BUFLEN;
+	int recvResult;
+	
 
-	do {
-		sendResult = recv(ClientSocket, recvbuf, DEFAULT_BUFLEN, 0);
-		if (sendResult > 0) 
+	while (1)
+	{
+		if ((ClientSocket = accept(ListenSocket, NULL, NULL)) != INVALID_SOCKET) 
 		{
-			printf("Bytes received: %d\n\n", sendResult);
-			// printf("recvbuf = %s", recvbuf);
-			// printf(httpHeader);
+			// accepted socket
+			recvResult = recv(ClientSocket, recvbuf, DEFAULT_BUFLEN, 0);
 
-			recieveResult = send(ClientSocket, httpHeader, sizeof(httpHeader), 0);
-			if (recieveResult == SOCKET_ERROR) 
+			if (recvResult > 0)
 			{
-				printf("send failed: %d\n", WSAGetLastError());
+				// recieved request
+				printf("Bytes received = %d\n", recvResult);
+				sendResult = send(ClientSocket, httpHeader, sizeof(httpHeader), 0);
+
+				if (sendResult != SOCKET_ERROR)
+				{
+					// send response
+					printf("Bytes sent = %d\n", sendResult);
+					closesocket(ClientSocket);
+				}
+				else
+				{
+					// error: failed response
+					printf("send failed: %d\n", WSAGetLastError());
+					closesocket(ClientSocket);
+					WSACleanup();
+					return 1;				
+				}
+			}
+			else if (recvResult == 0)
+			{
+				printf("Connection closing...\n");
+			}
+			else
+			{
+				// error: failed receive
+				printf("recv failed: %d\n", WSAGetLastError());
 				closesocket(ClientSocket);
 				WSACleanup();
 				return 1;
 			}
-			// printf("Bytes sent: %d\n", recieveResult);
-
-
-		}
-		else if (sendResult == 0)
-		{
-			printf("Connection closing...\n");
 		}
 		else
 		{
-			printf("recv failed: %d\n", WSAGetLastError());
-			closesocket(ClientSocket);
+			// error: rejected socket
+			printf("accept failed: %d\n", WSAGetLastError());
+			closesocket(ListenSocket);
 			WSACleanup();
 			return 1;
 		}
-
-		printf("enter e to exit\n");
-		a = getchar();
-		if (a == 101) break;
-
-	} while (sendResult > 0);
-
-
+	}
 
 	// shutdown the send half of the connection since no more data will be sent
-	int iResult = shutdown(ClientSocket, SD_SEND);
-	if (iResult == SOCKET_ERROR) 
+	if ((shutdown(ClientSocket, SD_SEND)) == SOCKET_ERROR) 
 	{
 		printf("shutdown failed: %d\n", WSAGetLastError());
 		closesocket(ClientSocket);
@@ -174,48 +155,38 @@ int main(int argc , char *argv[])
 	return 0;
 }
 
-int err()
-{
-	fprintf(stderr, "error = %d", WSAGetLastError());
-	WSACleanup();
-	return 1;
-}
-
 void setHttpHeader(char httpHeader[])
 {
-	printf("here");
-	FILE *htmlData = fopen("index.html", "r");
+	FILE *htmlData = fopen(filename, "r");
 
 	char line[100];
 	char responseData[8000];
 	while (fgets(line, 100, htmlData) != 0) 
 	{
-		printf(line);
 		strcat(responseData, line);
 	}
-
-	// strcat(httpHeader, " ");
 	strcat(httpHeader, responseData);
 }
 
+void report(struct sockaddr *serverAddress)
+{
+   char hostBuffer[INET6_ADDRSTRLEN];
+   char serviceBuffer[NI_MAXSERV];
 
-// void report(struct sockaddr_in *serverAddress)
-// {
-//     char hostBuffer[INET6_ADDRSTRLEN];
-//     char serviceBuffer[NI_MAXSERV];
-	
-//     socklen_t addr_len = (socklen_t) sizeof(*serverAddress);
+	int err = getnameinfo(
+		serverAddress, (socklen_t)sizeof(*serverAddress),
+		hostBuffer,    sizeof(hostBuffer), 
+		serviceBuffer, sizeof(serviceBuffer),
+		NI_NUMERICHOST
+	);
 
-//     int err = getnameinfo(
-//         (struct sockaddr *) serverAddress, addr_len,
-//         hostBuffer, sizeof(hostBuffer),
-//         serviceBuffer, sizeof(serviceBuffer),
-//         NI_NUMERICHOST
-//     );
+   if (err != 0) 
+	{
+   	printf("It's not working!!\n");
+   } 
+	else 
+	{
+		printf("\n\nServer listening on http://%s:%s\n\n", hostBuffer, serviceBuffer);
+	}
+}
 
-//     if (err != 0) {
-//         printf("It's not working!!\n");
-//     }
-
-//     printf("\n\n\tServer listening on http://%s:%s\n", hostBuffer, serviceBuffer);
-// }
